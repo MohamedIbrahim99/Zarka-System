08c6404eddd4cfab1158e2da71ebfc0de7136aa9
Mostafa Ahmed <59110598+MostafaAhmed660@users.noreply.github.com>
2022-09-07 17:26:18 +0200

    Add files via upload

---
+++ src/Node.java
@@ -0,0 +1,25 @@
+
+public class Node {
+    public String name ;
+    public String portName ;
+
+    public Node(String name) {
+        this.name = name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPortName() {
+        return portName;
+    }
+
+    public void setPortName(String portName) {
+        this.portName = portName;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
